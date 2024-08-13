8fb32048c68c4824f4ab4ec0db0eed4191bc1b22
unknown <katamalmberg04@gmail.com>
2024-08-13 13:58:09 +0200

    Changing Player Collision

--- Assets/script/FallingObject.cs
+++ Assets/script/FallingObject.cs
@@ -43,7 +43,7 @@ public class FallingObject : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.transform.tag == "Player")
+        if(collision.transform.tag == "Cart")
         {
             Debug.Log("hit player!");
             Instantiate(sound, transform.position, transform.rotation);
